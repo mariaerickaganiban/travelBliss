919ce974710d3a180415b939992ba6adee8971e9
mariaerickaganiban <109778775+mariaerickaganiban@users.noreply.github.com>
2023-07-24 12:55:55 +0800

    Add files via upload

---
+++ header.php
@@ -0,0 +1,49 @@
+<?php
+
+?>
+<html>
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
+}
+
+.active {
+  background-color: #04AA6D;
+}
+</style>
+
+    <header>
+        <ul>
+        <li><a href="1index.php">
+                <img src="TB-text.png" alt="Home Logo"  style="opacity: 1"  height="40" ></a></li>
+                <li><p class="p2"><a href="FAQs.php">FAQs</a></li>
+                <li style="float:right"><p class="p2"><a href="user_register.php">Registration</a></li>
+                <li style="float:right"><p class="p2"><a href="admin_login.php">Admin Login</a></li>
+               <li style="float:right"> <p class="p2"><a href="user_login.php">Member Login</a></li>
+                
+                
+        </ul>
+    </header >
+</html>
+
+
